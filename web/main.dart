55b57b225b52daac24ed2bf200f0a9da5d25beb0
icecreamtoo <2249253421@qq.com>
2016-11-10 16:14:34 +0800

    初始化项目

--- web/main.dart
+++ web/main.dart
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
 // Copyright (c) 2016, icecreamtoo. All rights reserved. Use of this source code
-=======
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
->>>>>>> origin/small_program
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
 
-InputElement toDoInput;
-UListElement toDoList;
-
 void main() {
-toDoInput = querySelector('#to-do-input');
-toDoList = querySelector('#to-do-list');
-toDoInput.onChange.listen(addToDoItem);
-}
-
-void addToDoItem(Event e) {
-var newToDo = new LIElement();
-newToDo.text = toDoInput.value;
-toDoInput.value = '';
-toDoList.children.add(newToDo);
+  querySelector('#output').text = 'Your Dart app is running.';
 }
