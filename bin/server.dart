ea9ac61c5bb12f66be594165b3c6ceb2b93aefea
suzy12345 <1010290852@qq.com>
2016-11-17 14:23:28 +0800

    server部分
    对服务器部分的修改

--- bin/server.dart
+++ bin/server.dart
@@ -6,17 +6,44 @@ void main() {
 
   var myRouter = router()
     ..get('/', responseRoot)
-    ..get('/stu', forstu);
+    ..get('/login', forlogin)
+    ..get('/no_login', forno_login)
+    ..get('/student/{stuid_x}', forstuid)
+    ..get('/teacher/{teaid_x}', forteaid)
+    ..get('/teacher/{teaid_x}/student/{stuid_m}', forteasearch);
+
 
   io.serve(myRouter.handler, '127.0.0.1', 8080);
 }
-
-forstu(request){
-
-  return new Response.ok("Hello of age ");
+///教师可根据学生的姓名学号，进行选择，从而知道学生的具体成绩信息
+forteasearch(request)
+{
+  return new Response.ok();
+}
+///教师输入自己的工号进行登录操作
+forteaid(request)
+{
+  return new Response.ok();
+}
+///学生输入自己的学号进行登录操作
+forstuid(request)
+{
+return new Response.ok();
+}
+///登录失败页面
+forno_login(request)
+{
+  return new Response.ok();
+}
+///登陆成功页面
+forlogin(request)
+{
+  return new Response.ok();
 }
 
-responseRoot(request){
+///欢迎进入奖学金评定系统
+responseRoot(request)
+{
   return new Response.ok("Hello World");
 }
 
