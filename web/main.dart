df41523cb382aa5911faaa709e678e633472fb49
icecreamtoo <2249253421@qq.com>
2016-11-17 16:58:47 +0800

    client_dart 修改了一些句子

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,34 @@
 
 import 'dart:html';
 
+String note;
+
+String login_yhm;//登录界面的用户名变量
+String login_mm;//登录界面的密码变量
+TextInputElement TextInput;
+ParagraphElement displayNote;
+TableElement displayTable;
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+///登陆界面
+  login_yhm= querySelector('#login_yhm'); //输入用户名
+  login_mm = querySelector('#login_mm');//输入密码
+
+  querySelector('#stulogin').onClick.listen(xsym);
+  querySelector('#tealogin').onClick.listen(jsym);
+}
+///用户点击登陆页面的学生登陆按钮的响应
+///学生主页面
+void xsym(MouseEvent e) {
+//todo 访问数据库，获取所有学生数据，并格式化为json
+  displayTable = querySelector('#stu_namenum');
+  displayTable = querySelector('#stushow');
+
+}
+///用户点击登陆页面的教师登陆按钮的响应
+///教师主页面
+void jsym(MouseEvent e) {
+//todo 访问数据库，获取所有学生数据，并格式化为json
+  displayTable = querySelector('#tea_namenum');
+  displayTable = querySelector('#teashow');
 }
